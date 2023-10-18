74e0921f9c5a859b92dc9e020e0813c58a0afe31
Rushikesh <3rushi@gmail.com>
2023-10-18 09:08:27 +0000

    c1

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "google" {
+  project = "tfgcptraining"
+  region = "us-east4"
+  zone = "us-east4-a"
+  
+}
+
+variable "var_vpcname" {
+  type = list
+  default = ["varpc1", "varpc2", "varpc3"]
+}
+
+resource "google_compute_network" "dep1" {
+  count = length(var.var_vpcname)
+  name = var.var_vpcname[count.index]
+  routing_mode = "GLOBAL"
+  auto_create_subnetworks = false
+  
+}
