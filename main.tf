a534c3f846373a7531d8b8daacd52d444affd7cb
Rushikesh <3rushi@gmail.com>
2023-10-18 09:23:04 +0000

    C1

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-provider "google" {
-  project = "tfgcptraining"
-  region = "us-east4"
-  zone = "us-east4-a"
+# provider "google" {
+#   project = "tfgcptraining"
+#   region = "us-east4"
+#   zone = "us-east4-a"
   
-}
+# }
 
 variable "var_vpcname" {
   type = list
@@ -15,5 +15,5 @@ resource "google_compute_network" "dep1" {
   name = var.var_vpcname[count.index]
   routing_mode = "GLOBAL"
   auto_create_subnetworks = false
-  
+
 }
